ef4b50dd557a4fc80de133b32341d1365dadf5b2
JoseVale99 <lc19160007@oaxaca.tecnm.mx>
2022-07-25 23:27:53 -0500

    ✅ Adding: solve problems

---
+++ python-tuples.py
@@ -0,0 +1,9 @@
+if __name__ == '__main__':
+    n = int(input())
+    integer_list = map(int, input().split())
+
+    tuple_list = tuple(integer_list)
+    print(hash(tuple_list).__abs__())
+
+    
+
